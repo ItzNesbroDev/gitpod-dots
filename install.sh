9fc77cb8fdfd7dea0206b27f169e67fb1fe3eaad
ItzNesbro Dev <asnesbeer3@gmail.com>
2023-05-07 07:48:19 +0000

    updated

--- install.sh
+++ install.sh
@@ -14,4 +14,20 @@ mv ~/dotfiles/.tm* ~/
 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 rm -rf ~/.zshrc
-mv ./zsh/.zshrc ~/.zshrc
+
+echo '# Define useful aliases
+alias cl="clear"
+alias ll="exa -l -g"
+alias ll="ll -a"
+alias g="git"
+
+# Install Zsh plugins
+plug "zap-zsh/supercharge"
+plug "zsh-users/zsh-autosuggestions"
+plug "zsh-users/zsh-syntax-highlighting" "122dc46"
+
+# Define prompt settings
+typeset -A __Prompt
+__Prompt[ITALIC_ON]=$'\e[3m'
+__Prompt[ITALIC_OFF]=$'\e[23m'
+plug "zap-zsh/singularisart-prompt"' >>~/.zshrc
